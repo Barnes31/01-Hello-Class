52d33b2d93a00047c41a37f68dcb799f9c10a986
Kayla Barnes <31224547+Barnes31@users.noreply.github.com>
2017-08-30 17:41:41 -0400

    Adds information about myself
    Alters the code in order to make it more relevant to me as a person.

--- 01 hello.py
+++ 01 hello.py
@@ -4,4 +4,17 @@ import sys
 
 assert sys.version_info >= (3,4), "This script requires at least Python 3.4"
 
-print('Hello, Class!')
+fav_game1 = " Skyrim,"
+fav_game2 = " Sims,"
+fav_game3 = " Guitar Hero"
+name = " Kayla"
+github = " https://github.com/Barnes31"
+print ("Hello, Class!")
+print ("My name is" + name + ".")
+print ("I love to game, but I haven't done so in a while.")
+print ("My usual go to games are" + fav_game1 + fav_game2 + " and" + fav_game3 + ".")
+print ("I'm excited to get to learn a new programming language, and I'm excited to begin creating my own content.")
+print ("Although the class seems like it is going to be really fun, I'm afraid that I won't be good at coding or that I'll be drowning in work.")
+print ("The URL to my github profile is" + github + ".")
+print ("And my Stack Overflow user number is 8497239.")
+
